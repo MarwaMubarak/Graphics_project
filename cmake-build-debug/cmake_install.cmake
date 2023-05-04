52654e4ac47ad415e383e32161159f20791a8ae4
Marwa Mubarak <marwamubarak6@gmail.com>
2023-05-05 02:56:44 +0300

    fix cmake

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/CLionProjects/GraphicsProject
+# Install script for directory: E:/StudioProjects/Graphics_project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/CLionProjects/GraphicsProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/StudioProjects/Graphics_project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
